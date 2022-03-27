f60a838a276c26f4a7a4df5a116de2d59f2c7690
franhace <13649860+franhace@users.noreply.github.com>
2022-03-27 12:32:31 -0300

    Labo2 - Recordatorios

--- Labo/ejercitacion_definicion_clases/src/Recordatorios.cpp
+++ Labo/ejercitacion_definicion_clases/src/Recordatorios.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 using uint = unsigned int;
@@ -27,6 +27,7 @@ class Fecha {
     #if EJ >= 9 // Para ejercicio 9
     bool operator==(Fecha o);
     #endif
+    void incrementar_dia();
 
   private:
     //Completar miembros internos
@@ -40,27 +41,109 @@ int Fecha::mes() {
     return mes_;
 }
 
+int Fecha::dia() {
+    return dia_;
+}
+
+ostream& operator << (ostream& os, Fecha f){
+    os << "" << f.dia() << "/" << f.mes() << "";
+}
 
 #if EJ >= 9
 bool Fecha::operator==(Fecha o) {
     bool igual_dia = this->dia() == o.dia();
+    bool igual_mes = this->mes() == o.mes();
     // Completar iguadad (ej 9)
-    return igual_dia;
+    return igual_dia && igual_mes;
 }
-
-
 
 #endif
 
+void Fecha::incrementar_dia() {
+    // caso que sea ultimo dia del mes
+    if (this->dia() == dias_en_mes(this->mes())){
+        dia_ = 1;
+        if (mes_==11){mes_=0;}
+        else {
+            mes_ = mes_ + 1;
+        }
+    } else{
+        dia_ = dia_ + 1;
+    }
+}
 // Ejercicio 11, 12
 
 // Clase Horario
+class Horario{
+public:
+    Horario(uint hora, uint min);
+    uint hora();
+    uint min();
+    bool operator == (Horario h);
+    bool operator < (Horario h);
 
+private:
+    int hora_;
+    int min_;
+};
 
+Horario::Horario(uint hora, uint min) : hora_(hora), min_(min){}
+
+uint Horario::hora() {
+    return hora_;
+}
+
+uint Horario::min() {
+    return min_;
+}
+
+bool Horario::operator==(Horario h) {
+    bool igual_hora = this->hora() == h.hora();
+    bool igual_min = this->min() == h.min();
+    return igual_hora && igual_min;
+}
+
+bool Horario::operator<(Horario h) {
+    return ((hora_ < h.hora()) or (hora_ == h.hora() && min_ < h.min()));
+}
+
+ostream& operator << (ostream& os, Horario h){
+    os << h.hora() << ":" << h.min();
+}
 // Ejercicio 13
 
 // Clase Recordatorio
+class Recordatorio{
+public:
+    Recordatorio(Fecha f, Horario h, string s);
+    string mensaje();
+    Horario horario();
+    Fecha fecha();
+private:
+    Horario h_;
+    Fecha f_;
+    string m_;
 
+
+};
+
+Recordatorio::Recordatorio(Fecha f, Horario h, string s) : f_(f), h_(h), m_(s) {}
+
+string Recordatorio::mensaje() {
+    return std::string(m_);
+}
+
+Horario Recordatorio::horario() {
+    return h_;
+}
+
+Fecha Recordatorio::fecha() {
+    return f_;
+}
+
+ostream& operator << (ostream& os, Recordatorio r){
+    os << r.mensaje() << " @ " << r.fecha() << " " << r.horario();
+}
 
 // Ejercicio 14
 
