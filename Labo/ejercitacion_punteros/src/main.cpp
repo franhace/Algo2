0d03cb0445a82a71ea93931c3116c9354624eaed
franhace <13649860+franhace@users.noreply.github.com>
2022-04-14 19:02:19 -0300

    LABO 4 - finito salvo ej7 bonux

---
+++ Labo/ejercitacion_punteros/src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ff on 9/4/22.
+//
+#include "SistemaDeMensajes.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SistemaDeMensajes sdm;
+    cout<<"Hello World";
+
+    return 0;
+}
