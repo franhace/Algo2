2bd46ccac3ff2c81163b972748b766b5e589c38f
franhace <13649860+franhace@users.noreply.github.com>
2022-05-29 18:57:21 -0300

    Actualizar labo 3

--- Labo/taller_conjunto_sobre_ABB/src/Conjunto.h
+++ Labo/taller_conjunto_sobre_ABB/src/Conjunto.h
@@ -10,72 +10,81 @@ using namespace std;
 template <class T>
 class Conjunto
 {
-    public:
+public:
 
-        // Constructor. Genera un conjunto vacío.
-        Conjunto();
+    // Constructor. Genera un conjunto vacío.
+    Conjunto();
 
-        // Destructor. Debe dejar limpia la memoria.
-        ~Conjunto();
+    // Destructor. Debe dejar limpia la memoria.
+    ~Conjunto();
 
-        // Inserta un elemento en el conjunto. Si este ya existe,
-        // el conjunto no se modifica.
-        void insertar(const T&);
+    // Inserta un elemento en el conjunto. Si este ya existe,
+    // el conjunto no se modifica.
+    void insertar(const T&);
 
-        // Decide si un elemento pertenece al conjunto o no.
-        bool pertenece(const T&) const;
+    // Decide si un elemento pertenece al conjunto o no.
+    bool pertenece(const T&) const;
 
-        // Borra un elemento del conjunto. Si este no existe,
-        // el conjunto no se modifica.
-        void remover(const T&);
+    // Borra un elemento del conjunto. Si este no existe,
+    // el conjunto no se modifica.
+    void remover(const T&);
 
-        // Siguiente elemento al recibido por párametro, en orden.
-        const T& siguiente(const T& elem);
+    // Siguiente elemento al recibido por párametro, en orden.
+    const T& siguiente(const T& elem);
 
-        // Devuelve el mínimo elemento del conjunto según <.
-        const T& minimo() const;
+    // Devuelve el mínimo elemento del conjunto según <.
+    const T& minimo() const;
 
-        // Devuelve el máximo elemento del conjunto según <.
-        const T& maximo() const;
+    // Devuelve el máximo elemento del conjunto según <.
+    const T& maximo() const;
 
-        // Devuelve la cantidad de elementos que tiene el conjunto.
-        unsigned int cardinal() const;
+    // Devuelve la cantidad de elementos que tiene el conjunto.
+    unsigned int cardinal() const;
 
-        // Muestra el conjunto.
-        void mostrar(std::ostream&) const;
+    // Muestra el conjunto.
+    void mostrar(std::ostream&) const;
 
-    private:
+    // Mostrar Inorder
+    void mostrar_inorder();
 
-        /**
-         * Completar con lo que sea necesario...
-         **/
-
-        struct Nodo
-        {
-            // El constructor, toma el elemento al que representa el nodo.
-            Nodo(const T& v) : valor(v),parent(NULL), izq(NULL), der(NULL) {};
-            // El elemento al que representa el nodo.
-            T valor;
-            // Puntero al padre.
-            Nodo* parent;
-            // Puntero a la raíz del subárbol izquierdo.
-            Nodo* izq;
-            // Puntero a la raíz del subárbol derecho.
-            Nodo* der; 
-        };
-
-        // Puntero a la raíz de nuestro árbol.
-        Nodo* _raiz;
-
-        // Cantidad de elems en abb
-        int _cardinal;
-
+private:
+    /*** Completar con lo que sea necesario... ***/
+    struct Nodo
+    {
+        // El constructor, toma el elemento al que representa el nodo.
+        Nodo(const T& v) : valor(v), parent(NULL), izq(NULL), der(NULL) {};
+        // El elemento al que representa el nodo.
+        T valor;
+        // Puntero al nodo padre
+        Nodo* parent; // parent
+        // Puntero a la raíz del subárbol izquierdo.
+        Nodo* izq;
+        // Puntero a la raíz del subárbol derecho.
+        Nodo* der;
+    };
+    // Puntero a la raíz de nuestro árbol.
+    Nodo* _raiz;
+    // AGREGO:
+    // Cantidad de nodos en el conjunto
+    int _cardinal;
+    // busca un valor y devuelve un puntero al nodo del conjunto.
+    // si no existe, devuelve NULL
+    Nodo* buscar(const T&) const;
+    //
+    void transplantar(Nodo* u, Nodo* v);
+    //
+    void inorder_tree_walk_search_next(Nodo* nodo, const T& clave);
+    T _elQueSigue;
+    // Para mostrar
+    void inorder_tree_walk(Nodo* nodo);
+    // Para borrar
+    void inorder_tree_destruction(Nodo* nodo);
 };
 
 template<class T>
 std::ostream& operator<<(std::ostream& os, const Conjunto<T>& c) {
-	 c.mostrar(os);
-	 return os;
+    c.mostrar(os);
+    return os;
 }
 
 #include "Conjunto.hpp"
--- Labo/taller_conjunto_sobre_ABB/src/Conjunto.hpp
+++ Labo/taller_conjunto_sobre_ABB/src/Conjunto.hpp
@@ -1,67 +1,179 @@
+
 template <class T>
-Conjunto<T>::Conjunto(): _raiz() {
-    // Completar
+Conjunto<T>::Conjunto() : _raiz(NULL), _cardinal(0), _elQueSigue(NULL) {
 }
 
 template <class T>
-Conjunto<T>::~Conjunto() { 
+void Conjunto<T>::inorder_tree_destruction(Nodo* nodo){
+    //From Cormen's book
+    if(nodo != NULL){
+        inorder_tree_destruction(nodo->izq);
+
+        inorder_tree_destruction(nodo->der);
+        // Delete en orden
+        delete(nodo);
+    }
+}
+
+template <class T>
+Conjunto<T>::~Conjunto() {
     // Completar
+    inorder_tree_destruction(_raiz);
 }
 
 template <class T>
 bool Conjunto<T>::pertenece(const T& clave) const {
     Nodo* nodo = _raiz;
-    while (nodo != nullptr && nodo->valor != clave){
-        if (clave > nodo->valor) nodo = nodo->der;
-        else nodo = nodo->izq;
-    }
-    return nodo != nullptr;
-}
-
-template <class T>
-void Conjunto<T>::insertar(const T& clave) {
-    if (pertenece(clave)== false){
-        Nodo* nuevo = new Nodo(clave);
-        Nodo* anterior = NULL;
-        Nodo* nodo = _raiz;
-
-        while (nodo != NULL){
-            anterior = nodo;
-            if (nuevo->valor < nodo->valor){
-                nodo = nodo->izq;
-            } else {
-                nodo = nodo->der;
-            }
-        }
-        nuevo->parent = anterior;
-        if (anterior == NULL){
-            // ABB vacio
-            _raiz = nuevo;
-        } else if (nuevo->valor < anterior->valor){
-            anterior->izq = nuevo;
+    //From Cormen's book
+    //cout << "Pertenece " << clave << " ? : " ;
+    while (nodo != NULL && nodo->valor != clave){
+        //cout << "nodo->valor:" << nodo->valor << endl;
+        //cout << "nodo->izq:" << nodo->izq << endl;
+        //cout << "nodo->der:" << nodo->der << endl;
+        if (clave < nodo->valor){
+            nodo = nodo->izq;
         } else {
-            anterior->der = nuevo;
+            nodo = nodo->der;
         }
+    }
+    //cout << (nodo != NULL) << endl;
+    return nodo != NULL;
+}
 
-        _cardinal += 1;
+template<class T>
+void Conjunto<T>::insertar(const T& clave) {
+    if (pertenece(clave)){
+        // Elemento ya existe
+        return;
     }
 
+    Nodo* nuevo = new Nodo(clave);
+    Nodo* anterior = NULL;
+    Nodo* nodo = _raiz;
+    //From Cormen's book
+    while (nodo != NULL){
+        anterior = nodo;
+        if (nuevo->valor < nodo->valor){
+            nodo = nodo->izq;
+        } else {
+            nodo = nodo->der;
+        }
+    }
+    nuevo->parent = anterior;
+    if (anterior == NULL){
+        // Arbol vacio
+        _raiz = nuevo;
+    } else if (nuevo->valor < anterior->valor){
+        anterior->izq = nuevo;
+    } else {
+        anterior->der = nuevo;
+    }
+
+    _cardinal += 1;
+
+    return;
 }
 
 template <class T>
-void Conjunto<T>::remover(const T&) {
-    assert(false);
+void Conjunto<T>::transplantar(Nodo* u, Nodo* v) {
+    if(u->parent == NULL){
+        _raiz = v;
+    } else if (u == u->parent->izq){
+        u->parent->izq = v;
+    } else {
+        u->parent->der = v;
+    }
+    if (v != NULL){
+        v->parent = u->parent;
+    }
+}
+
+template <class T>
+void Conjunto<T>::remover(const T& clave) {
+    // Si existe, 3 casos:
+    // * Es hoja
+    // * Tiene un hijo
+    // * Tiene dos hijos
+    Nodo* nodo = _raiz;
+    //From Cormen's book
+    // recorre el arbol hasta encontrar nodo->valor == clave
+    while (nodo != NULL && (nodo->valor) != clave){
+        if (clave < nodo->valor){
+            nodo = nodo->izq;
+        } else {
+            nodo = nodo->der;
+        }
+    }
+    if (nodo == NULL){
+        // No pertenece al conjunto
+        return;
+    }
+
+    if (nodo->izq == NULL){
+//        cout << "NULL a izquierda" << endl;
+        transplantar(nodo, nodo->der);
+    } else if (nodo->der == NULL) {
+//        cout << "NULL a derecha" << endl;
+        transplantar(nodo, nodo->izq);
+    } else {
+//        cout << "Ambos hijes" << endl;
+        // Busco el nodo minimo
+        Nodo* minimo = nodo->der;
+
+        //From Cormen's book
+        while (minimo->izq != NULL){
+            minimo = minimo->izq;
+//            cout << "3. minimo en el while: " << minimo->valor << endl;
+//            cout << "3. minimo parent en el while: " << minimo->parent->valor << endl;
+        }
+//        cout << "Minimo > value: " << minimo->valor << endl;
+//        cout << "Minimo > parent > value: " << minimo->parent->valor << endl;
+//        cout << "nodo > value: " << nodo->valor << endl;
+        if (minimo->parent != nodo){
+//            cout << "minimo->parent != nodo" << endl;
+            transplantar(minimo, minimo->der);
+            minimo->der = nodo->der;
+            minimo->der->parent = minimo;
+        }
+        transplantar(nodo, minimo);
+        minimo->izq = nodo->izq;
+//        cout << "Luego de transplante, nodo>izq>valor " << nodo -> izq -> valor << endl;
+        minimo->izq->parent = minimo;
+
+    }
+
+    delete(nodo);
+    _cardinal -= 1;
+}
+
+template <class T>
+void Conjunto<T>::inorder_tree_walk_search_next(Nodo* nodo, const T& clave) {
+    //From Cormen's book
+    if(nodo != NULL){
+        inorder_tree_walk_search_next(nodo->izq, clave);
+        //cout << "Elem: " << (nodo->valor) << endl;
+        if(_elQueSigue == NULL && (nodo->valor > clave)){
+            _elQueSigue = nodo->valor;
+            return;
+        }
+        inorder_tree_walk_search_next(nodo->der, clave);
+    }
 }
 
 template <class T>
 const T& Conjunto<T>::siguiente(const T& clave) {
-    assert(false);
+    _elQueSigue = NULL;
+    Nodo* nodo = _raiz;
+    //cout << "El que sigue a " << clave << endl;
+    inorder_tree_walk_search_next(nodo, clave);
+    return _elQueSigue;
 }
 
 template <class T>
 const T& Conjunto<T>::minimo() const {
     Nodo* nodo = _raiz;
-    while (nodo != nullptr){
+    //From Cormen's book
+    while (nodo->izq != NULL){
         nodo = nodo->izq;
     }
     return nodo->valor;
@@ -70,12 +182,12 @@ const T& Conjunto<T>::minimo() const {
 template <class T>
 const T& Conjunto<T>::maximo() const {
     Nodo* nodo = _raiz;
-    while (nodo != nullptr){
+    //From Cormen's book
+    while (nodo->der != NULL){
         nodo = nodo->der;
     }
     return nodo->valor;
 }
-
 
 template <class T>
 unsigned int Conjunto<T>::cardinal() const {
@@ -83,14 +195,22 @@ unsigned int Conjunto<T>::cardinal() const {
 }
 
 template <class T>
-void Conjunto<T>::mostrar(std::ostream&) const {
-    std::cout << "[";
-    Nodo* actual = _raiz;
-    while (actual->next) {
-        std::cout << actual->value << ", ";
-        actual = actual->next;
-    }
-    std::cout << actual->value << "]";
+void Conjunto<T>::mostrar_inorder(){
+    inorder_tree_walk(_raiz);
 }
 
+template <class T>
+void Conjunto<T>::inorder_tree_walk(Nodo* nodo){
+    //From Cormen's book
+    if(nodo != NULL){
+        inorder_tree_walk(nodo->izq);
+        cout << (nodo->valor) << endl;
+        inorder_tree_walk(nodo->der);
+    }
+}
+
+template <class T>
+void Conjunto<T>::mostrar(std::ostream&) const {
+    inorder_tree_walk(_raiz);
+}
 
